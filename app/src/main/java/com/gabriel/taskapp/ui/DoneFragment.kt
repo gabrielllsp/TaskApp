7d508bb53211dd42d4545a198147cfaed6c50a6d
gabrielllsp <gabriel.amoreira@outlook.com>
2023-08-21 12:49:18 -0300

    Criando e configurando o tablayout.

---
+++ app/src/main/java/com/gabriel/taskapp/ui/DoneFragment.kt
@@ -0,0 +1,23 @@
+package com.gabriel.taskapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gabriel.taskapp.databinding.FragmentDoneBinding
+
+
+class DoneFragment : Fragment() {
+
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
