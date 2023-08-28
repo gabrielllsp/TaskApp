7d2d609365592db7223cb3d3de939ba28d5615e7
gabrielllsp <gabriel.amoreira@outlook.com>
2023-08-28 15:44:42 -0300

    Implementando o firebase, salvando o cadastro do usuário e verificando se o usuário já esa logado.

--- app/src/main/java/com/gabriel/taskapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/gabriel/taskapp/ui/auth/RegisterFragment.kt
@@ -5,16 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gabriel.taskapp.R
 import com.gabriel.taskapp.databinding.FragmentRegisterBinding
 import com.gabriel.taskapp.util.initToolbar
 import com.gabriel.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +33,9 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
+        auth = Firebase.auth
         initListeners()
+
     }
 
     private fun initListeners() {
@@ -41,13 +49,28 @@ class RegisterFragment : Fragment() {
 
         if (email.isNotEmpty()) {
             if (password.isNotEmpty()) {
-                Toast.makeText(requireContext(), "Tudo certo.", Toast.LENGTH_LONG).show()
+                binding.progressBar.isVisible = true
+                registerUser(email, password)
             } else {
                 showBottomSheet(message = getString(R.string.password_empty_register_fragment))
             }
         } else {
             showBottomSheet(message = getString(R.string.email_empty_register_fragment))
         }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+
+                } else {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
     }
 
     override fun onDestroy() {
